293b8bda380792d6acd97d2216af31761fa91b56
Unknown <tvwijgerden@hotmail.com>
2018-12-16 20:13:33 +0100

    Added example database for import

---
+++ src/main/resources/database.sql
@@ -0,0 +1,140 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Gegenereerd op: 16 dec 2018 om 20:12
+-- Serverversie: 10.1.31-MariaDB
+-- PHP-versie: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `playlist`
+--
+
+CREATE TABLE `playlist` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `playlist`
+--
+
+INSERT INTO `playlist` (`id`, `name`) VALUES
+(1, 'Playlist 1'),
+(2, 'Playlist 2');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `playlist_track`
+--
+
+CREATE TABLE `playlist_track` (
+  `playlist_id` int(11) NOT NULL,
+  `track_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `playlist_track`
+--
+
+INSERT INTO `playlist_track` (`playlist_id`, `track_id`) VALUES
+(1, 1),
+(1, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `track`
+--
+
+CREATE TABLE `track` (
+  `id` int(11) NOT NULL,
+  `path` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `track`
+--
+
+INSERT INTO `track` (`id`, `path`) VALUES
+(1, 'example/path/1'),
+(2, 'example/path/2'),
+(3, 'example/path/3'),
+(4, 'example/path/4'),
+(5, 'example/path/5'),
+(6, 'example/path/6'),
+(7, 'example/path/7'),
+(8, 'example/path/8'),
+(9, 'example/path/9'),
+(10, 'example/path/10');
+
+--
+-- Indexen voor geëxporteerde tabellen
+--
+
+--
+-- Indexen voor tabel `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `playlist_track`
+--
+ALTER TABLE `playlist_track`
+  ADD PRIMARY KEY (`playlist_id`,`track_id`),
+  ADD KEY `IDX_75FFE1E56BBD148` (`playlist_id`),
+  ADD KEY `IDX_75FFE1E55ED23C43` (`track_id`);
+
+--
+-- Indexen voor tabel `track`
+--
+ALTER TABLE `track`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT voor geëxporteerde tabellen
+--
+
+--
+-- AUTO_INCREMENT voor een tabel `playlist`
+--
+ALTER TABLE `playlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT voor een tabel `track`
+--
+ALTER TABLE `track`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `playlist_track`
+--
+ALTER TABLE `playlist_track`
+  ADD CONSTRAINT `FK_75FFE1E55ED23C43` FOREIGN KEY (`track_id`) REFERENCES `track` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `FK_75FFE1E56BBD148` FOREIGN KEY (`playlist_id`) REFERENCES `playlist` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
