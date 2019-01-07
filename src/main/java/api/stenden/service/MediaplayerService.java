bed245341ab4a2b968968fe60c689a07dee1a847
Matthew Elderhorst <matthew.elderhorst@student.stenden.com>
2019-01-07 13:09:37 +0100

    Make mediaplayer branch

---
+++ src/main/java/api/stenden/service/MediaplayerService.java
@@ -0,0 +1,30 @@
+package api.stenden.service;
+
+import jaco.mp3.player.MP3Player;
+
+import java.io.File;
+
+
+public class MediaplayerService {
+
+ public static void main(String[] args){
+     PlayerThread player_thread = new PlayerThread();
+     player_thread.start();
+ }
+}
+
+class PlayerThread extends Thread{
+    MP3Player mp3Player;
+
+    public void run(){
+        try {
+            mp3Player = new MP3Player(new File("resource/Cage The Elephant - Trouble.mp3"));
+            mp3Player.play();
+            while(!mp3Player.isStopped()){
+                Thread.sleep(5000);
+            }
+        }catch(Exception e) {
+            System.err.println(e);
+        }
+    }
+}
