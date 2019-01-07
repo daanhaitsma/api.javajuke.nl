b6791acd2c3b546bad5f59135dc4bd26db23f6bb
Unknown <tvwijgerden@hotmail.com>
2019-01-07 13:28:13 +0100

    Imporved exceptions

--- src/main/java/api/javajuke/exception/RestExceptionHandler.java
+++ src/main/java/api/javajuke/exception/RestExceptionHandler.java
@@ -5,15 +5,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<Object> handleException(EntityNotFoundException ex, WebRequest request) {
-        ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, ex.getMessage(), request.getDescription(false));
+    public ResponseEntity<Object> handleEntityNotFoundException(EntityNotFoundException ex) {
+        ApiError apiError = new ApiError(HttpStatus.NOT_FOUND, ex.getMessage(), 404);
+
+        return new ResponseEntity<>(apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<Object> handleBadRequestException(MethodArgumentTypeMismatchException ex) {
+        ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage(), 400);
 
         return new ResponseEntity<>(apiError, new HttpHeaders(), apiError.getStatus());
     }
