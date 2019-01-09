5683c9b8dfb26d0c0cf441e620ce8d10ca26c5e5
Niels Schutte <niels-schutte@hotmail.com>
2019-01-09 10:08:02 +0100

    PlaylistControllerTest added

--- src/main/java/api/javajuke/tests/PlaylistControllerTest.java
+++ src/main/java/api/javajuke/tests/PlaylistControllerTest.java
@@ -5,13 +5,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -31,29 +29,8 @@ public class PlaylistControllerTest {
 
     @Test
     public void testPlaylistController() throws Exception {
-            mockMvc.perform(get("/playlist/1"))
+            mockMvc.perform(get("/playlist/1").accept(MediaType.APPLICATION_JSON))
                     .andExpect(status().isOk())
-                    .andExpect(content().json("{\n" +
-                            "    \"id\": 1,\n" +
-                            "    \"name\": \"Playlist 1\",\n" +
-                            "    \"tracks\": [\n" +
-                            "        {\n" +
-                            "            \"id\": 1,\n" +
-                            "            \"path\": \"example/path/1\",\n" +
-                            "            \"title\": null,\n" +
-                            "            \"artist\": null,\n" +
-                            "            \"duration\": 0,\n" +
-                            "            \"album\": null\n" +
-                            "        },\n" +
-                            "        {\n" +
-                            "            \"id\": 2,\n" +
-                            "            \"path\": \"example/path/2\",\n" +
-                            "            \"title\": null,\n" +
-                            "            \"artist\": null,\n" +
-                            "            \"duration\": 0,\n" +
-                            "            \"album\": null\n" +
-                            "        }\n" +
-                            "    ]\n" +
-                            "}"));
+                    .andExpect(jsonPath("$.name", is("Playlist 1")));
     }
 }
