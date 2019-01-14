c5f4170d3416ed2d14e8ca40e29c01d45babfcf2
Unknown <tvwijgerden@hotmail.com>
2019-01-14 13:56:01 +0100

    Updated PlaylistController comments

--- src/main/java/api/javajuke/res/PlaylistController.java
+++ src/main/java/api/javajuke/res/PlaylistController.java
@@ -40,6 +40,8 @@ public class PlaylistController implements VersionController{
     /**
      * Creates an endpoint that creates a new playlist.
      *
+     * @param body the request body containing POST data
+     * @param token the X-Authorization header value which contains the user token
      * @return the newly created playlist as a json response
      */
     @PostMapping(value = "/playlists", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
@@ -52,7 +54,9 @@ public class PlaylistController implements VersionController{
     /**
      * Creates an endpoint that shows a playlist with the specified id.
      *
+     * @param id the playlist id
      * @return the playlist as a json response
+     * @throws EntityNotFoundException when the playlist is not found
      */
     @GetMapping("/playlists/{id}")
     public Playlist show(@PathVariable("id") long id) throws EntityNotFoundException {
@@ -62,7 +66,11 @@ public class PlaylistController implements VersionController{
     /**
      * Creates an endpoint that updates an existing playlist with the specified id.
      *
+     * @param id the playlist id
+     * @param body the request body containing POST data
      * @return the updated playlist as a json response
+     * @throws EntityNotFoundException when the playlist is not found
+     * @throws BadRequestException when an invalid request body is sent
      */
     @PutMapping(value = "/playlists/{id}", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     public Playlist update(@PathVariable("id") long id, @RequestBody MultiValueMap<String, String> body) throws EntityNotFoundException, BadRequestException {
@@ -73,6 +81,10 @@ public class PlaylistController implements VersionController{
 
     /**
      * Creates an endpoint that deletes a playlist with the specified id.
+     *
+     * @param token the X-Authorization header value which contains the user token
+     * @param id the playlist id
+     * @throws EntityNotFoundException when the playlist is not found
      */
     @DeleteMapping("/playlists/{id}")
     public void delete(@RequestHeader(value = "X-Authorization") String token, @PathVariable("id") long id) throws EntityNotFoundException{
@@ -82,6 +94,8 @@ public class PlaylistController implements VersionController{
     /**
      * Creates an endpoint that adds a track to a playlist.
      *
+     * @param id the playlist id
+     * @param trackId the track id
      * @return the updated playlist as a json response
      */
     @PostMapping(value = "/playlists/{id}/tracks/{trackId}")
@@ -92,6 +106,8 @@ public class PlaylistController implements VersionController{
     /**
      * Creates an endpoint that deletes a track from a playlist.
      *
+     * @param id the playlist id
+     * @param trackId the track id
      * @return the updated playlist as a json response
      */
     @DeleteMapping(value = "/playlists/{id}/tracks/{trackId}")
