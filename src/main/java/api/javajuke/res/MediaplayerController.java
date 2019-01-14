a68a2d984b036a2976bf2476791c7d74dc669946
daanhaitsma <daan@haitsma-dejager.nl>
2019-01-14 12:32:47 +0100

    Remove methods and classes that have been replaced by PlayerState

--- src/main/java/api/javajuke/res/MediaplayerController.java
+++ src/main/java/api/javajuke/res/MediaplayerController.java
@@ -1,14 +1,7 @@
 package api.javajuke.res;
 
 import api.javajuke.data.model.PlayerState;
-import api.javajuke.data.model.Position;
-import api.javajuke.data.model.Volume;
-import api.javajuke.exception.BadRequestException;
-import api.javajuke.exception.EntityNotFoundException;
 import api.javajuke.service.MediaplayerService;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
@@ -48,11 +41,6 @@ public class MediaplayerController {
         mediaplayerService.previousSong();
     }
 
-    @GetMapping("/shuffle")
-    public void getShuffle(){
-        mediaplayerService.getShuffle();
-    }
-
     @PutMapping("/shuffle")
     public void toggleShuffle(){
         mediaplayerService.toggleShuffle();
@@ -67,20 +55,6 @@ public class MediaplayerController {
     public void setVolume(@RequestBody MultiValueMap<String, String> body){
         String volume = body.getFirst("volume");
         mediaplayerService.setVolume(Integer.valueOf(volume));
-    }
-
-    @GetMapping("/volume")
-    public Volume getVolume(){
-        return new Volume(mediaplayerService.getVolume());
-    }
-
-    @GetMapping("/position")
-    public Position getPosition(){
-        if (mediaplayerService.isPlaying() || mediaplayerService.isPaused()) {
-            return new Position(mediaplayerService.getPosition());
-        } else {
-            return new Position(0);
-        }
     }
     
     @GetMapping("/addsong")
